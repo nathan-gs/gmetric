1c43a034ede252065f1814ce3203ecf2435eadbd
Nathan Bijnens <nathan@nathan.gs>
2012-01-12 23:02:32 +0100

    full path to smartctl

--- health/smartctl/smartctl.sh
+++ health/smartctl/smartctl.sh
@@ -9,15 +9,15 @@ HARDDISKS=`find /sys/block/ -name 'sd*' -printf "%f\n"`
 GMETRIC=/usr/bin/gmetric
 
 for disk in $HARDDISKS; do
-	Temp=`smartctl -A /dev/$disk | grep Temperature_Celsius | awk '{print $10}'`
-#	Airflow_Temp=`smartctl -A /dev/$disk | grep Airflow_Temperature_Cel | awk '{print $10}'`
+	Temp=`/usr/sbin/smartctl -A /dev/$disk | grep Temperature_Celsius | awk '{print $10}'`
+#	Airflow_Temp=`/usr/sbin/smartctl -A /dev/$disk | grep Airflow_Temperature_Cel | awk '{print $10}'`
 
 	$GMETRIC -t float -n "disk_${disk}_temp" -u "C" -v $Temp
 #	$GMETRIC -t float -n "disk_${disk}_air_temp" -u "C" -v $Airflow_Temp
 done
 
-Max_Temp=`for disk in \`find /sys/block/ -name 'sd*' -printf "%f\n"\`; do smartctl -A /dev/${disk} | grep Temperature_Celsius | awk '{print $10}'; done | sort -r | head -1`
-#Max_Airflow_Temp=`for disk in `find /sys/block/ -name 'sd*' -printf "%f\n"`; do smartctl -A /dev/${disk} | grep Airflow_Temperature_Cel | awk '{print $10}'; done | sort -r | head -1`
+Max_Temp=`for disk in \`find /sys/block/ -name 'sd*' -printf "%f\n"\`; do /usr/sbin/smartctl -A /dev/${disk} | grep Temperature_Celsius | awk '{print $10}'; done | sort -r | head -1`
+#Max_Airflow_Temp=`for disk in `find /sys/block/ -name 'sd*' -printf "%f\n"`; do /usr/sbin/smartctl -A /dev/${disk} | grep Airflow_Temperature_Cel | awk '{print $10}'; done | sort -r | head -1`
 $GMETRIC -t float -n "disk_temp" -u "C" -v $Max_Temp
 #$GMETRIC -t float -n "disk_air_temp" -u "C" -v $Max_Airflow_Temp
 
